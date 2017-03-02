5a09052a201a0be4555134db11ae8a6a88b2be8a
vanny00 <jh722698@dal.ca>
2017-03-02 09:47:06 -0400

    EmailValidator: added more conditions.
    ValTest: added two new tests for empty and string maxlength.

--- src/main/java/com/email_validator/email_validator/EmailValidator.java
+++ src/main/java/com/email_validator/email_validator/EmailValidator.java
@@ -1,10 +1,28 @@
 package com.email_validator.email_validator;
 
-public class EmailValidator{
-	
-	boolean validate(String email)
-	{
-		return false;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator 
+{
+	/* researched http://stackoverflow.com/questions/8204680/java-regex-email
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+		public  boolean validate(String email) {
+		        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(email);
+		        return matcher.find();
+		}*/
+	boolean validate(String email){
+		if(email.contains("@") && email.contains(".")
+				&& email.length()<255 && !email.contains(" "))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+			
 	}
-	
 }
--- src/test/java/com/email_validator/email_validator/ValTest.java
+++ src/test/java/com/email_validator/email_validator/ValTest.java
@@ -16,6 +16,19 @@ public class ValTest extends TestCase
 		boolean success = app.validate("van");
 		assertFalse(success);
 	}
+	public void testEmpty()
+	{
+		EmailValidator app = new EmailValidator();
+		boolean success = app.validate(" ");
+		assertFalse(success);
+	}
+	//String is 255 characters long			
+		public void test2long()
+	{
+			EmailValidator app = new EmailValidator();
+			boolean success = app.validate("JtpZAfBx4e0mWXtwibvMWCzQTgvSXybaYkXwl8soKgnseqaibf8yS2bvQRWrFmo0zxMsVLalcG1emUYbzXMOSjoK3x2fMF4tZbyyPZkjFZPPLWr5guTJ1LWQc0oi7IWh1qMU9muA3leOecOuhyZTwcGxLs4EGDBATJewfzoJRHMPwWB1AonSAvXyBE6H4MUieDI64L2P66UhIsKKKAMFtC58T85xHiI9WRunujVYs8yAZqopEBzbaZm8qtyhiUO");
+			assertFalse(success);
+	}
 	
 	
 	
